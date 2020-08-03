3f8d3c7827a60c535b558be13796ba281ce35c25
lvxu2020 <lvxu2020@163.com>
2020-08-02 23:55:37 -0700

    test

--- cat/App/moc_mainwindow.cpp
+++ cat/App/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[11];
-    char stringdata0[157];
+    QByteArrayData data[12];
+    char stringdata0[178];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,17 +38,19 @@ QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 14), // "slot_scanfOver"
 QT_MOC_LITERAL(4, 40, 23), // "slot_wifiConnectChanged"
 QT_MOC_LITERAL(5, 64, 11), // "std::string"
-QT_MOC_LITERAL(6, 76, 19), // "on_WIFIScan_clicked"
-QT_MOC_LITERAL(7, 96, 19), // "on_listWIFI_clicked"
-QT_MOC_LITERAL(8, 116, 11), // "QModelIndex"
-QT_MOC_LITERAL(9, 128, 5), // "index"
-QT_MOC_LITERAL(10, 134, 22) // "on_connectWIFI_clicked"
+QT_MOC_LITERAL(6, 76, 20), // "slot_netStatusChange"
+QT_MOC_LITERAL(7, 97, 19), // "on_WIFIScan_clicked"
+QT_MOC_LITERAL(8, 117, 19), // "on_listWIFI_clicked"
+QT_MOC_LITERAL(9, 137, 11), // "QModelIndex"
+QT_MOC_LITERAL(10, 149, 5), // "index"
+QT_MOC_LITERAL(11, 155, 22) // "on_connectWIFI_clicked"
 
     },
     "MainWindow\0sig_scanWIFI\0\0slot_scanfOver\0"
     "slot_wifiConnectChanged\0std::string\0"
-    "on_WIFIScan_clicked\0on_listWIFI_clicked\0"
-    "QModelIndex\0index\0on_connectWIFI_clicked"
+    "slot_netStatusChange\0on_WIFIScan_clicked\0"
+    "on_listWIFI_clicked\0QModelIndex\0index\0"
+    "on_connectWIFI_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -58,7 +60,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -66,14 +68,15 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x06 /* Public */,
+       1,    0,   49,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    1,   45,    2, 0x08 /* Private */,
-       4,    1,   48,    2, 0x08 /* Private */,
-       6,    0,   51,    2, 0x08 /* Private */,
-       7,    1,   52,    2, 0x08 /* Private */,
-      10,    0,   55,    2, 0x08 /* Private */,
+       3,    1,   50,    2, 0x08 /* Private */,
+       4,    1,   53,    2, 0x08 /* Private */,
+       6,    1,   56,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    1,   60,    2, 0x08 /* Private */,
+      11,    0,   63,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
@@ -81,8 +84,9 @@ static const uint qt_meta_data_MainWindow[] = {
  // slots: parameters
     QMetaType::Void, QMetaType::Bool,    2,
     QMetaType::Void, 0x80000000 | 5,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 8,    9,
+    QMetaType::Void, 0x80000000 | 9,   10,
     QMetaType::Void,
 
        0        // eod
@@ -97,9 +101,10 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->sig_scanWIFI(); break;
         case 1: _t->slot_scanfOver((*reinterpret_cast< bool(*)>(_a[1]))); break;
         case 2: _t->slot_wifiConnectChanged((*reinterpret_cast< std::string(*)>(_a[1]))); break;
-        case 3: _t->on_WIFIScan_clicked(); break;
-        case 4: _t->on_listWIFI_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 5: _t->on_connectWIFI_clicked(); break;
+        case 3: _t->slot_netStatusChange((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 4: _t->on_WIFIScan_clicked(); break;
+        case 5: _t->on_listWIFI_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 6: _t->on_connectWIFI_clicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -143,13 +148,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 7;
     }
     return _id;
 }
--- cat/App/moc_netMonitor.cpp
+++ cat/App/moc_netMonitor.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_NetMonitor_t {
-    QByteArrayData data[7];
-    char stringdata0[105];
+    QByteArrayData data[8];
+    char stringdata0[124];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,14 +36,16 @@ QT_MOC_LITERAL(0, 0, 10), // "NetMonitor"
 QT_MOC_LITERAL(1, 11, 17), // "monitorTimerStart"
 QT_MOC_LITERAL(2, 29, 0), // ""
 QT_MOC_LITERAL(3, 30, 16), // "monitorTimerStop"
-QT_MOC_LITERAL(4, 47, 12), // "slot_monitor"
-QT_MOC_LITERAL(5, 60, 22), // "slot_monitorTimerStart"
-QT_MOC_LITERAL(6, 83, 21) // "slot_monitorTimerStop"
+QT_MOC_LITERAL(4, 47, 18), // "sig_netSatusChange"
+QT_MOC_LITERAL(5, 66, 12), // "slot_monitor"
+QT_MOC_LITERAL(6, 79, 22), // "slot_monitorTimerStart"
+QT_MOC_LITERAL(7, 102, 21) // "slot_monitorTimerStop"
 
     },
     "NetMonitor\0monitorTimerStart\0\0"
-    "monitorTimerStop\0slot_monitor\0"
-    "slot_monitorTimerStart\0slot_monitorTimerStop"
+    "monitorTimerStop\0sig_netSatusChange\0"
+    "slot_monitor\0slot_monitorTimerStart\0"
+    "slot_monitorTimerStop"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,25 +55,27 @@ static const uint qt_meta_data_NetMonitor[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       2,       // signalCount
+       3,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x06 /* Public */,
-       3,    0,   40,    2, 0x06 /* Public */,
+       1,    0,   44,    2, 0x06 /* Public */,
+       3,    0,   45,    2, 0x06 /* Public */,
+       4,    1,   46,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       5,    0,   49,    2, 0x08 /* Private */,
+       6,    0,   50,    2, 0x08 /* Private */,
+       7,    0,   51,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    2,
 
  // slots: parameters
     QMetaType::Void,
@@ -89,9 +93,10 @@ void NetMonitor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->monitorTimerStart(); break;
         case 1: _t->monitorTimerStop(); break;
-        case 2: _t->slot_monitor(); break;
-        case 3: _t->slot_monitorTimerStart(); break;
-        case 4: _t->slot_monitorTimerStop(); break;
+        case 2: _t->sig_netSatusChange((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 3: _t->slot_monitor(); break;
+        case 4: _t->slot_monitorTimerStart(); break;
+        case 5: _t->slot_monitorTimerStop(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -110,8 +115,14 @@ void NetMonitor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
                 return;
             }
         }
+        {
+            using _t = void (NetMonitor::*)(bool );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&NetMonitor::sig_netSatusChange)) {
+                *result = 2;
+                return;
+            }
+        }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject NetMonitor::staticMetaObject = { {
@@ -143,13 +154,13 @@ int NetMonitor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
@@ -164,6 +175,13 @@ void NetMonitor::monitorTimerStart()
 void NetMonitor::monitorTimerStop()
 {
     QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void NetMonitor::sig_netSatusChange(bool _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
